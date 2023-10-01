43ddae524f2ce944a8c96673e76b895b1e88186d
MohammadTahaHussain <hussaintaha620@gmail.com>
2023-10-01 17:29:14 +0500

    10/8 completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,31 @@
-// window.addEventListener("scroll", function () {
-//     var sections = document.querySelectorAll("section");
-//     var navLinks = document.querySelectorAll("nav ul li a");
+const ytLinks = [
+    'https://www.youtube.com/embed/e1gJhwy2RQI?si=z4Ao1P45WYXmRiy0',
+    'https://www.youtube.com/embed/hAPpilOfD9U?si=gQLKCAzKyRQJfxs9',
+    'https://www.youtube.com/embed/a1r2UAz0bdI?si=gbLN7vIsjDD7CaqO',
+    'https://www.youtube.com/embed/zRmDDDjJhRc?si=3o2_jcUjQxE5ZLQR',
+    'https://www.youtube.com/embed/lWWJBM1jFiU?si=fsFHhP1dbVuPe9dg',
+    'https://www.youtube.com/embed/Iq1OcbvwbIE?si=_7WY_9-FNkxzrUX5',
+    'https://www.youtube.com/embed/Th-8YgMKmeA?si=Colm5vwhFLwNLN74',
+    'https://www.youtube.com/embed/awxsLMgpiZg?si=x-YfGqi7e9LysLDQ',
+    'https://www.youtube.com/embed/d3B6DvQSlyE?si=lzHKgXoRcYpQNIdb',
+    'https://www.youtube.com/embed/EfZQLhzrfs8?si=6nLnFAdkh7A292c8',
+    'https://www.youtube.com/embed/RILE91KoZxw?si=mcYLibY5ziAX03Xn',
+    'https://www.youtube.com/embed/4qrtpGLMVsE?si=zebmy6lvwrf4sjrk'
+]
 
-//     sections.forEach(function (section, index) {
-//         var top = section.offsetTop;
-//         var height = section.offsetHeight;
-//         var offset = window.pageYOffset + 120;
-//         var id = section.getAttribute("id");
+const ytContainer = document.getElementById('yt-links')
 
-//         if (offset >= top && offset < top + height) {
-//             // navLinks[index].style.color = "rgb(29, 78, 216)";
-//             navLinks[index].classList.add("block")
-//             navLinks[index].classList.add("py-2")
-//             navLinks[index].classList.add("pl-3")
-//             navLinks[index].classList.add("pr-4")
-//             navLinks[index].classList.add("text-white")
-//             navLinks[index].classList.add("bg-blue-700")
-//             navLinks[index].classList.add("rounded")
-//             navLinks[index].classList.add("md:bg-transparent")
-//             navLinks[index].classList.add("md:text-blue-700")
-//             navLinks[index].classList.add("md:p-0")
-//             navLinks[index].classList.add("dark:text-white")
-//             navLinks[index].classList.add("md:dark:text-blue-500")
-//         } else {
-//             while (navLinks[index].classList.length > 0) {
-//                 navLinks[index].classList.remove(navLinks[index].classList.item(0));
-//               }
-//             navLinks[index].classList.add("block")
-//             navLinks[index].classList.add("py-2")
-//             navLinks[index].classList.add("pl-3")
-//             navLinks[index].classList.add("pr-4")
-//             navLinks[index].classList.add("text-gray-500")
-//             navLinks[index].classList.add("hover:bg-gray-100")
-//             navLinks[index].classList.add("md:hover:bg-transparent")
-//             navLinks[index].classList.add("md:border-0")
-//             navLinks[index].classList.add("md:hover:text-blue-700")
-//             navLinks[index].classList.add("md:p-0")
-//             navLinks[index].classList.add("dark:text-white")
-//             navLinks[index].classList.add("md:dark:hover:text-blue-500")
-//             navLinks[index].classList.add("dark:hover:bg-gray-700")
-//             navLinks[index].classList.add("dark:hover:text-white")
-//             navLinks[index].classList.add("md:dark:hover:bg-transparent")
-//         }
-//     });
-// });
+function updateYt() {
+    ytContainer.innerHTML = ""
+    ytLinks.forEach(e => {
+        ytContainer.innerHTML += `<div>
+        <iframe class="w-[320px] lg:w-[420px] xl:w-[560px] h-[315px]"
+            src =${e}
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+    </div>`
+    })
+}
+
+updateYt()
